68cd6ee7463a474cd24914479829c26e999daa0f
Dipak giri <dipakgiri1200@gmail.com>
2021-06-24 10:03:37 +0545

    first cmmit

---
+++ app.py
@@ -0,0 +1,141 @@
+# print('hello world')
+
+# python variables 
+  # one=1
+  # two=2
+  # some_number=1000
+  # print(one,two,some_number)
+
+# booleans 
+  # true_boolean=True
+  # false_boolean=False
+
+  # # string
+  # my_name='diapk'
+
+  # # float 
+  # book_price=12.3
+  # print(true_boolean,false_boolean,my_name,book_price)
+
+# if True:
+#   print('Hello python learners:')
+
+# if 3>1:
+#   print('3 is greater than 1')
+
+
+# control flow and conditional statements
+    # if 1>2:
+    #   print('1 is greater than 2')
+    # elif 2>1:
+    #   print('2 is greater than 1')
+    # else:
+    #   print('1 is equal to 2')
+
+# looping and iterator 
+    # num=1
+    # while num<=100:
+    #   print(num)
+    #   num+=1
+
+# another basic bit of code to better understand it :
+    # loop_condition=True
+
+    # while loop_condition:
+    #   print('loop condition keeps: %s' %(loop_condition))
+    #   loop_condition=False
+
+# for looping 
+# for i in range(1,11):
+#   print(i)
+
+
+# list collection |array and data structure 
+
+  # my_integers=[1,2,3,4,5,6,7,8,9]
+  # print(my_integers[0])
+  # print(my_integers[2])
+  # print(my_integers[3])
+  # print(my_integers[5])
+  # print(my_integers[4])
+  # print(my_integers[6])
+
+
+    # relatives_names=[
+    #   'bipana',
+    #   'dipak',
+    #   'jagat',
+    #   'ananda',
+    #   'premlal',
+    #   'saraswati'
+    # ]
+    # for i in range(0,len(relatives_names)):
+    #   print(relatives_names[i])
+
+
+# appending the list in to the empty array 
+      # book_list=[]
+      # book_list.append('The effective enginners')
+      # book_list.append('money heist')
+      # book_list.append('the mekemoney dev')
+      # book_list.append('the python study')
+      # book_list.append('the javascript learners')
+
+      # print(book_list[4])
+
+      # for i in range(0,len(book_list)):
+      #   print(book_list[i])
+
+# dictoionary_example 
+    # dictoionary_example={
+    #   "key1":"value1",
+    #   "key2":"value2",
+    #   "key3":"value3"
+    # }
+
+    # print(dictoionary_example)
+    # print(dictoionary_example["key1"])
+    # print(dictoionary_example["key2"])
+
+
+# printting the dictoionary in the formatted way 
+# dictoionary_tk={
+#   "name":"DIPAK",
+#   "age":21,
+#   "education":"bachelore"
+# }
+
+
+# dictoionary_tk["age"]=23;
+# print(dictoionary_tk)
+
+
+# bookshef=[
+#   "the effective engineer",
+#   "The 4 hours of social network",
+#   "zero to mestry",
+#   "the Hero"
+#   ]
+
+# for book in bookshef:
+#   print(book)
+
+# dictionary = { "some_key": "some_value" }
+
+# for key in dictionary:
+#     print("%s --> %s" %(key, dictionary[key]))
+
+
+# dictionary = { "some_key": "some_value" }
+
+# for key, value in dictionary.items():
+#   print("%s --> %s" %(key,value))
+
+
+# dictoionary_tk={
+#   "name":"DIPAK",
+#   "age":21,
+#   "education":"bachelore"
+# }
+# for attribute, value in dictoionary_tk.items():
+#   print('my %s is %s' %(attribute,value))
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+print('hello iam %s iam %s years old and iam studying %s from college of applied business.' %(dictoionary_tk["name"] %(dictoionary_tk[age] %(dictoionary_tk["education"]))))
